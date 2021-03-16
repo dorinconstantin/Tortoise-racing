fcee1afe60d28cfad5843bc8170f81ce8fe4c435
Dorin Constantin <dorinilieconstantin@gmail.com>
2021-03-16 08:35:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tortoise_racing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Tortoise.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tortoise_racing
+{
+    public class Tortoise
+    {
+        public static int[] Race(int v1, int v2, int g)
+        {
+            if (v1 >= v2) return null;
+            var t = TimeSpan.FromHours((double)g / (v2 - v1));
+            return new[] { t.Hours, t.Minutes, t.Seconds };
+        }
+    }
+}
